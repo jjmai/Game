7f7d96cd7eeabbab11bf9cb3ff8b6e3aa8a826b9
Jordan Mai <jmai12@unix1.lt.ucsc.edu>
2020-12-21 14:46:31 -0800

    win condition

--- header.cpp
+++ header.cpp
@@ -5,47 +5,78 @@
 using namespace std;
 
 void play::table() {
-  char board[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
+  char board[3][3] = {{'0', '1', '2'}, {'3', '4', '5'}, {'6', '7', '8'}};
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
       cout << board[i][j] << ' ';
     }
-    cout << "\n";
+    cout << "\n\n";
   }
 }
 
 int play::playing() {
   int turn = rand() % 2;
-  int p1;
-  int p2;
-
-  if (turn == 0) {
-    cout << "Enter a Valid Position on The graph. You are 'X'\n\n";
-
-    cin >> p1;
-    cout << "\n\n";
-
-    if (p1 > 0 && p1 <= 9) {
-      while (true) {
-        p2 = rand() % 9 + 1;
-        if (p2 != p1) {
-          break;
-        }
-      }
-      for (int i = 1; i < 10; i++) {
-        if (p1 == i) {
-          cout << "X ";
-        } else if (p2 == i) {
-          cout << "O ";
-        } else {
-          cout << "-" << " ";
-        }
-        if (i > 0 && i % 3 == 0) {
-          cout << "\n";
-        }
-      }
+  int number = 0, x = 0, y = 0;
+  char board[3][3];
+  initialize(board);
+  print_board(board);
+  while (true) {
+    
+    cin >> number;
+    x = number / 3;
+    y = number % 3;
+    board[x][y] = 'X';
+    bot(board);
+    print_board(board);
+    if (check(board)) {
+      break;
     }
   }
-  cout << "\n";
-  return 1;
+  cout <<"DONE";
+}
+
+void play::bot(char board[][3]) {
+  int x = 0, y = 0;
+  while (true) {
+    x = rand() % 3;
+    y = rand() % 9;
+    if (board[x][y] == '-') {
+      board[x][y] = 'O';
+      break;
+    }
+  }
+  //cout <<"DONE";
+}
+
+bool play::check(char board[][3]) {
+  for (int i = 0; i < 3; i++) {
+    if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != '-') {
+      return true;
+    } else if (board[0][i] == board[1][i] && board[1][i]== board[2][i] &&
+               board[0][i] != '-') {
+      return true;
+    } else if (board[0][0] == board[1][1]&& board[1][1] == board[2][2] &&
+               board[0][0] != '-') {
+      return true;
+    }
+  }
+  
+  return false;
+}
+
+void play::initialize(char board[][3]) {
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      board[i][j] = '-';
+    }
+  }
+}
+
+void play::print_board(char board[][3]) {
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      cout << board[i][j] << " ";
+    }
+    cout << "\n\n";
+  }
 }
--- header.h
+++ header.h
@@ -8,6 +8,10 @@ public:
   void player1();
   void player2();
   int playing();
+  void bot(char board[][3]);
+  bool check(char board[][3]);
+  void initialize(char board[][3]);
+  void print_board(char bpard[][3]);
 };
 
 #endif
